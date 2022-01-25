3db9b5aa40dcade61ec2145d6a8ac7efc9e83099
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2022-01-25 16:48:54 -0300

    Upload da aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,25 @@
+import React from 'react';
+import { useRouter } from 'next/router';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-
-      #__next {
-        flex: 1;
-      }
-
-      #__next > * {
-        flex: 1;
-      }
-    `}</style>
-  )
-}
-
-function Title(props) {
-  const Tag = props.tag || 'h1';
-  return(
-    <>
-      <Tag>{props.children}</Tag>
-
-      <style jsx>{`
-        ${Tag} {
-          color: ${appConfig.theme.colors.neutrals['000']};
-          font-size: 24px;
-          font-weight: 600;
-        } 
-      `}</style>
-    </>
-  );
-}
-
 export default function HomePage() {
-  const username = 'gabrielcs04';
+  const [ username, setUsername ] = React.useState('gabrielcs04');
+  const routing = useRouter();
 
   return (
     <>
-      <GlobalStyle />
+
       <Box
         styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
           backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          backgroundRepeat: 'no-repeat', 
+          backgroundSize: 'cover',
+          backgroundBlendMode: 'multiply',
         }}
       >
         <Box
@@ -68,12 +27,12 @@ export default function HomePage() {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-between',
-            flexDirection: {
-              xs: 'column',
-              sm: 'row',
-            },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
+            flexDirection: { xs: 'column', sm: 'row' },
+            width: '100%', 
+            maxWidth: '700px',
+            borderRadius: '5px', 
+            padding: '32px', 
+            margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
             backgroundColor: appConfig.theme.colors.neutrals[700],
           }}
@@ -81,17 +40,46 @@ export default function HomePage() {
 
           <Box
             as="form"
+            onSubmit={(event) => {
+              event.preventDefault();
+              routing.push('/chat');
+            }}
             styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              display: 'flex', 
+              flexDirection: 'column', 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' }, 
+              textAlign: 'center',
+              marginBottom: '32px',
             }}
           >
-            <Title tag="h2">Boas vindas de volta!</Title>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text 
+              variant="body3" 
+              styleSheet={{ 
+                fontSize: '24px', 
+                fontWeight: 'bold', 
+                color: appConfig.theme.colors.neutrals["000"] 
+              }}
+            >
+              Boas vindas de volta!
+            </Text>
+            <Text 
+              variant="body3" 
+              styleSheet={{ 
+                marginBottom: '32px', 
+                color: appConfig.theme.colors.neutrals[300] 
+              }}
+            >
               {appConfig.name}
             </Text>
 
             <TextField
+              value={username}
+              onChange={(event) => {
+                const valor = event.target.value;
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
